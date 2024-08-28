bc0dd5594f2fb2b5dfe28acc9b42d65e2e9d77e4
yuchienchen <yuchienc@yahoo.com>
2024-08-28 12:47:36 -0500

    Introduce class

--- circle.py
+++ circle.py
@@ -2,19 +2,29 @@ import math
 
 r = 5
 
-def get_area():
-    # r = float(input("Radius: "))
-    return math.pi * r ** 2.0
+class Circle:
+    ...
 
-def get_circumference():
-    # r = float(input("Radius: "))
-    return 2.0 * math.pi * r
+# def get_area():
+#     # r = float(input("Radius: "))
+#     return math.pi * r ** 2.0
+
+# def get_circumference():
+#     # r = float(input("Radius: "))
+#     return 2.0 * math.pi * r
+
+def get_circle():
+    circle = Circle()
+    circle.area = math.pi * r ** 2.0
+    circle.circumference = 2.0 * math.pi * r
+    return circle
     
 def main(): 
-    area = get_area()
-    circumference = get_circumference()
-    print(area)
-    print(circumference)
+    circle = get_circle()
+    # area = get_area()
+    # circumference = get_circumference()
+    print(circle.area)
+    print(circle.circumference)
 
 
 if __name__ == "__main__":
